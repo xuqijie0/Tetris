2292f7f71eeb14d400503484f175d6f1f390c6df
xuqijie <xlocks06@gmail.com>
2024-02-08 18:51:54 +0800

    demo

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main () {
 
     tc::move_to(5, 10);
     tc::set_fore_color(214);
-    std::cout << "Hello World"<<std::endl;
-    tc::move_to(10,1);
+    std::cout << "Hello World" << std::endl;
+    tc::move_to(10, 1);
     return 0;
 }
