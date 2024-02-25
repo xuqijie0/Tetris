fbdf578b825b925c1be25ad916ae2a40b5eb2510
xuqijie <xlocks06@gmail.com>
2024-02-25 23:22:20 +0800

    pc编辑

---
+++ draw.cpp
@@ -0,0 +1,26 @@
+#include "draw.h"
+#include "terminal.h"
+
+
+/*
+unicode box
+ 	    0	1	2	3	4	5	6	7	8	9	A	B	C	D	E	F
+U+250x	─	━	│	┃	┄	┅	┆	┇	┈	┉	┊	┋	┌	┍	┎	┏
+U+251x	┐	┑	┒	┓	└	┕	┖	┗	┘	┙	┚	┛	├	┝	┞	┟
+U+252x	┠	┡	┢	┣	┤	┥	┦	┧	┨	┩	┪	┫	┬	┭	┮	┯
+U+253x	┰	┱	┲	┳	┴	┵	┶	┷	┸	┹	┺	┻	┼	┽	┾	┿
+U+254x	╀	╁	╂	╃	╄	╅	╆	╇	╈	╉	╊	╋	╌	╍	╎	╏
+U+255x	═	║	╒	╓	╔	╕	╖	╗	╘	╙	╚	╛	╜	╝	╞	╟
+U+256x	╠	╡	╢	╣	╤	╥	╦	╧	╨	╩	╪	╫	╬	╭	╮	╯
+U+257x	╰	╱	╲	╳	╴	╵	╶	╷	╸	╹	╺	╻	╼	╽	╾	╿
+
+*/
+
+inline int block2col(int b) { return 2 * b - 1; }
+
+void dw::window(int top, int left, int height, std::string title) 
+{
+  tc::move_to(top, block2col(left));
+  
+
+}
---
+++ draw.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+/*
+
+ui: laout
+dw: draw
+tc: terminal contral
+gm: game logic
+ut: utils
+
+
+*/
+
+namespace dw {
+void window(int top, int left, int height, std::string title);
+}
